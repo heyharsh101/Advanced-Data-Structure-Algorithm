fbe902b6f7f3507168973a4d8ed42c5939491bde
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-10-15 01:03:25 +0530

    DSA

--- LeetCode/Stack/20-Valid Parenthesis/main.java
+++ LeetCode/Stack/20-Valid Parenthesis/main.java
@@ -2,4 +2,29 @@ import java.util.Scanner;
 
 public class main {
     
+    public boolean isValid(String s) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Input = sc.nextLine();
+        
+        main main = new main();
+        boolean result = main.isValid(Input);
+        System.out.println(result);
+        sc.close();
+    }
 }
