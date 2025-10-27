1e61b843d29e78427ce2da62ac3214e8cf5c830c
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-10-27 23:40:21 +0530

    DSA

---
+++ LeetCode/Array/7-Reverse Integer/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 3;
+        int[][] result = solution.generateMatrix(n);
+        
+        
+    }
+}
