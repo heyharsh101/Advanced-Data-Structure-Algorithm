3b1382c216830816defc02ac54d89bf54d84a690
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-10-27 23:43:48 +0530

    DSA

--- LeetCode/Array/7-Reverse Integer/main.java
+++ LeetCode/Array/7-Reverse Integer/main.java
@@ -1,11 +1,33 @@
+// Using simple arithmetic to reverse the integer
+// Handling overflow by checking before multiplying
 
 public class main {
-    
+    public static int reverse(int x) {
+        int rev =0;
+
+        while(x != 0){
+            int digit = x % 10;
+            x /= 10;
+            
+            // Check overflow before multiplying
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && digit > 7)) {
+            return 0;
+            }
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int n = 3;
-        int[][] result = solution.generateMatrix(n);
+        main solution = new main();
+        int n = -123;
+        int result = main.reverse(n);
         
+        System.out.println("Reversed Integer: " + result);
         
     }
 }
