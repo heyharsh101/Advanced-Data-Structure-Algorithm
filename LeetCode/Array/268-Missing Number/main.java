cc458fac9eb53cbdad3e1782961e688580d16c9c
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-10-30 23:49:39 +0530

    DSA

---
+++ LeetCode/Array/268-Missing Number/main.java
@@ -0,0 +1,18 @@
+
+public class main {
+    public int missingNumber(int[] nums){
+        int n= nums.length;
+        int sum = n*(n+1)/2;
+        int count =0;
+        for(int i=0; i<n; i++){
+            count = count + nums[i];
+        }
+        return sum - count;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,0,1};
+        main obj = new main();
+        int result = obj.missingNumber(nums);
+        System.out.println(result);
+    }
+}
