e758a9c0fedad771a09d00fb79ac9aecd76c5b57
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-10-31 23:26:54 +0530

    DSA

---
+++ LeetCode/Array/125-Valid Palindrome/main.java
@@ -0,0 +1,28 @@
+public class main {
+    public boolean isPalindrome(String s) {
+        int left =0;
+        int right = s.length()-1;
+        while (left <right) {
+            char leftChar =s.charAt(left);
+            char rightChar =s.charAt(right);
+            if (!Character.isLetterOrDigit(leftChar)){
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)){
+                right--;
+            } else {
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = ("A man, a plan, a canal: Panama");
+        main obj = new main();
+        boolean result = obj.isPalindrome(s);
+        System.out.println(result);
+    }
+}
