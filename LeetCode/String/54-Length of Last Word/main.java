9e4fcf7f8540e8fb86916f47cb2714ce0295e34c
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-11-03 20:51:25 +0530

    DSA

---
+++ LeetCode/String/54-Length of Last Word/main.java
@@ -0,0 +1,21 @@
+
+public class main {
+    public static int lengthOfLastWord(String s){
+        int length =0;
+
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i)!= ' '){
+                length++;
+            } else if(length > 0){
+                break;
+            }
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello World ";
+        int result = lengthOfLastWord(s);
+        System.out.println(result);
+    }
+}
