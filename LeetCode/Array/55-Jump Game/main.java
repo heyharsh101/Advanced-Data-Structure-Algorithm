cf1da47720c77eff62a14e722db4a55d0c941d6d
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-11-07 23:50:59 +0530

    DSA

--- LeetCode/Array/55-Jump Game/main.java
+++ LeetCode/Array/55-Jump Game/main.java
@@ -1,3 +1,9 @@
+// Use mathematical greedy approach to determine if you can reach the last index of the array.
+// Main :- Keep track of the maximum reachable index as you iterate through the array.
+// If at any point the current index exceeds the maximum reachable index, return false.
+// If you can iterate through the array without exceeding the maximum reachable index, return true.
+// Time Complexity :- O(n), where n is the length of the input array.
+// Space Complexity :- O(1), as we are using only a constant amount of extra space.
 
 public class main {
     public static boolean canJump(int[] nums) {
