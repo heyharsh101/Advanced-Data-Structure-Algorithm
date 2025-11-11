48c7a7aeb100c5068073c6d4735607f0d776868c
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-11-11 23:28:50 +0530

    DSA

---
+++ LeetCode/Tree/100-Same Tree/main.java
@@ -0,0 +1,40 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class main{
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+    }
+
+    public static void main(String[] args) {
+        // create tree nodes and test the isSameTree function here if needed
+        TreeNode p1 = new TreeNode(1);
+        p1.left = new TreeNode(2);
+        p1.right = new TreeNode(3);
+        TreeNode q1 = new TreeNode(1);
+        
+        q1.left = new TreeNode(2);
+        q1.right = new TreeNode(3);
+        main solution = new main();
+        System.out.println(solution.isSameTree(p1, q1)); 
+    }
+}
