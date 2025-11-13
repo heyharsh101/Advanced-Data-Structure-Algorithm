b624968ab17066e5a5e04c52ed6938ebbe8692cd
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-11-13 23:55:20 +0530

    DSA

---
+++ LeetCode/Tree/226-Invert Binary Tree/main.java
@@ -0,0 +1,41 @@
+
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+public class main {
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        TreeNode invertedRoot = invertTree(root);
+        // You can add code here to print or verify the inverted tree if needed.
+        
+
+    }
+}
