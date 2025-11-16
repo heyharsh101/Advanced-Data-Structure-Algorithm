eea750fee599c20d416bd45162d296a8437a722f
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-11-16 23:31:38 +0530

    DSA

---
+++ LeetCode/Array/169-Majority Element/main.java
@@ -0,0 +1,22 @@
+public class main {
+    public static int majorityElement(int[] nums) {
+        int count =0;
+        int candidate =0;
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+            }
+            if(num==candidate){
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {3,2,3};
+        System.out.println(majorityElement(nums));
+    }
+}
