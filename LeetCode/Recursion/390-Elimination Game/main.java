6d1dc206aeec36a819bc1bebdc85806b708ea084
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-11-24 23:50:23 +0530

    DSA

---
+++ LeetCode/Recursion/390-Elimination Game/main.java
@@ -0,0 +1,23 @@
+
+public class main {
+    public boolean predictTheWinner(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = winner(nums, left, right);
+        return result>=0;
+    }
+    public int winner(int[] nums, int left , int right){
+        if(left == right){
+            return nums[left];
+        }
+        int takeleft = nums[left] - winner(nums, left + 1, right);
+
+        int takeright = nums[right] - winner(nums, left, right - 1);
+
+        return Math.max(takeleft, takeright);
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {}
+    }
+}
