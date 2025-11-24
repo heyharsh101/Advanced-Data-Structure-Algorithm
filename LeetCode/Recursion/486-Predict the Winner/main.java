1abb5a1610a3ff6d9376a5c3a4c3b715d0730fca
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-11-24 23:48:04 +0530

    DSA

---
+++ LeetCode/Recursion/486-Predict the Winner/main.java
@@ -0,0 +1,25 @@
+
+public class main {
+    public static boolean predictTheWinner(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = winner(nums, left, right);
+        return result>=0;
+    }
+    public static int winner(int[] nums, int left , int right){
+        if(left == right){
+            return nums[left];
+        }
+        int takeleft = nums[left] - winner(nums, left + 1, right);
+
+        int takeright = nums[right] - winner(nums, left, right - 1);
+
+        return Math.max(takeleft, takeright);
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 2};
+        main main = new main();
+        boolean ans = predictTheWinner(nums);
+        System.out.println(ans);
+    }
+}
