661e2998772a6b268c9cd1ce2ec971a42baba656
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-12-04 23:52:10 +0530

    DSA

--- LeetCode/DP/198-House Robber/main.java
+++ LeetCode/DP/198-House Robber/main.java
@@ -1,3 +1,23 @@
 public class main {
-    
+    public static int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n==1) return nums[0];
+
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
+
+        for(int i=2; i < n; i++){
+            int rob = nums[i] + prev2;
+            int skip = prev1;
+            int curr = Math.max(rob, skip);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+
+
+    }
 }
