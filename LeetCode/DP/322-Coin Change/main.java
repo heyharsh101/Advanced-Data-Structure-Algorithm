923d6f37fd09f560122d4fafe24c7203650ea83f
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-12-05 23:42:24 +0530

    DSA

--- LeetCode/DP/322-Coin Change/main.java
+++ LeetCode/DP/322-Coin Change/main.java
@@ -1,5 +1,25 @@
-package LeetCode.DP.322-Coin Change;
 
 public class main {
-    
+    public static int coinChange(int[] coins, int amount) {
+        int INF = (int)1e9;
+
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, INF);
+
+        dp[0]=0;
+
+        for(int i=1; i<=amount; i++){
+            for(int coin : coins){
+                if(i-coin >=0){
+                    dp[i] = Math.min(dp[i], 1+dp[i-coin]);
+                }
+            }
+        }
+
+        if(dp[amount]==INF){
+            return -1;
+        } else {
+            return dp[amount];
+        }
+    }
 }
