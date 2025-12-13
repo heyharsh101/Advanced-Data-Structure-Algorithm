06fcf8de75dd7d0075eac29b3273c85eadedb569
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-12-13 21:51:27 +0530

    DSA

--- LeetCode/Array/1006-Clumsy Factorial/main.java
+++ LeetCode/Array/1006-Clumsy Factorial/main.java
@@ -26,4 +26,11 @@ public class main {
         }
         return result;
     }
+    public static void main(String[] args) {
+        main solution = new main();
+        int[] tests = {4, 10, 1, 2, 3, 5, 6, 7, 8, 9};
+        for (int t : tests) {
+            System.out.println("n=" + t + " -> " + solution.clumsy(t));
+        }
+    }
 }
