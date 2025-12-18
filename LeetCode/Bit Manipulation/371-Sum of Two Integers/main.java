f7c87b250eb637db367b6af4b258631269ff4cc5
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-12-18 22:57:49 +0530

    DSA

---
+++ LeetCode/Bit Manipulation/371-Sum of Two Integers/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static int getSum(int a, int b) {
+        while(b != 0){
+            int carry = (a & b) << 1;
+            a = a ^b;
+            b = carry;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        main m = new main();
+        int a = 1;
+        int b = 2;
+        System.out.println(m.getSum(a, b));
+    }
+}
