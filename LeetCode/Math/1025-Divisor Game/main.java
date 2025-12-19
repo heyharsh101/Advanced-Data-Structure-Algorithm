8f8fe6afbc39364facfcf64610aa284fde2a2ef6
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2025-12-20 02:34:02 +0530

    DSA

--- LeetCode/Math/1025-Divisor Game/main.java
+++ LeetCode/Math/1025-Divisor Game/main.java
@@ -1,7 +1,11 @@
 // simple game if alice is first and n is even she will always win
 public class main {
-    public boolean divisorGame(int n) {
-        return n % 2 == 0;
+    public static boolean divisorGame(int n) {
+        if(n % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
     public static void main(String[] args) {
         main m = new main();
