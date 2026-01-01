3d45978ab555e6a5777c2e3f7008a68eb7c60848
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2026-01-01 23:58:01 +0530

    DSA

---
+++ LeetCode/Linkedlist/203-Remove Linked List Elements/main.java
@@ -0,0 +1,46 @@
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        this.val = val;
+        this.next = null;
+    }
+}
+public class main {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy;
+        while(curr.next != null){
+            if(curr.next.val == val){
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+            
+        }
+        return dummy.next;
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next.next = new ListNode(6);
+
+        int val = 6;
+
+        main obj = new main();
+        ListNode result = obj.removeElements(head, val);
+
+        // Print the modified linked list
+        ListNode curr = result;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+}
