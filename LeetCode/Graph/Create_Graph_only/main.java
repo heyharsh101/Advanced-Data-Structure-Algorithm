36bc1bd2df7c758eeedbe084024e16283feb6af3
heyharsh101 <161753130+heyharsh101@users.noreply.github.com>
2026-01-07 00:05:46 +0530

    DSA

---
+++ LeetCode/Graph/Create_Graph_only/main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+class Edge {
+    int src;
+    int dest;
+    int weight;
+
+    public Edge(int s, int d, int w){
+        this.src = s;
+        this.dest = d;
+        this.weight = w;
+    }
+}
+
+
+public class main {
+    public static ArrayList<Edge> createGraph(ArrayList<Edge> edges, int V) {
+    ArrayList<Edge> graph = new ArrayList<>();
+
+        graph.add(new Edge(0, 1, 10));
+        graph.add(new Edge(0,2, 15));
+        graph.add(new Edge(1, 3,20));
+        graph.add(new Edge(2,3, 25));
+        return graph;
+    }
+
+    public static void print(ArrayList<Edge> graph){
+        for(Edge e : graph){
+            System.out.println(e.src + "--" + e.dest + ":" + e.weight);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Edge> graph = new ArrayList<>();
+        print(graph);
+    }
+}
--- LeetCode/Linkedlist/92-Reverse Linked List II/main.java
+++ LeetCode/Linkedlist/92-Reverse Linked List II/main.java
@@ -22,7 +22,6 @@ public class main {
         for(int i=0; i<left -1; i++){
             prev = prev.next;
         }
-
         ListNode curr = prev.next;
         for(int i=0; i<right-left; i++){
             ListNode nextNode = curr.next;
